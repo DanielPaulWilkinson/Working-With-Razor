19f5118cb1adc53625df7cc224be148ba6e7adc5
Daniel Wilkinson <danielpaulwilkinson.developer@gmail.com>
2017-12-29 17:40:36 +0000

    Completed Sub-Chapter: Creating the view and working with model object  - page 109

---
+++ WorkingWithRazor/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WorkingWithRazor.Models;
+
+namespace WorkingWithRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product Kayak = new Product
+            {
+                Name = "Kayak",
+                Desc = "A one man boat.",
+                price = 10,
+                ProductID = 1,
+                Category = "Boat"
+        
+            };
+            return View(Kayak);
+        }
+    }
+}
---
+++ WorkingWithRazor/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkingWithRazor.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set;  }
+        public string Desc { get; set; }
+        public decimal price { get; set; }
+        public string Category { get; set; }
+    }
+}
